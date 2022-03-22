4e11399d1de19bdcb44bfd612669f7b5bc662bec
Abdul Awal Suhail <abdulawalsuhail>
2022-03-22 21:11:26 +0600

    product add to cart container and tax tofixed

--- src/component/cart/Cart.js
+++ src/component/cart/Cart.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart}) => {
+
+    let total=0;
+    let shipping=0;
+    for(const product of cart){
+        total= total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax =(total*0.1).toFixed(2);
     return (
-        <div>
-             <h2>Order summary</h2>
+        <div className='cart'>
+             <h2 className='order'>Order summary</h2>
              <p>Selected items:{cart.length}</p>
+             <p>Total Price:${total}</p>
+             <p>Total Shipping Charge:${shipping}</p>
+             <p>Tax:${tax}</p>
+             <h3>Grand Total:</h3>
         </div>
     );
 };
